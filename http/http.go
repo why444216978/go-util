b56c059e47a7cbdc5d728eb38ff1b3150d76600b
weihaoyu <weihaoyu@ymt360.com>
2021-02-11 15:25:17 +0800

    http

--- http/http.go
+++ http/http.go
@@ -2,82 +2,59 @@ package http
 
 import (
 	"bytes"
-	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
-	"strings"
-
-	"github.com/why444216978/go-util/conversion"
 
 	"github.com/pkg/errors"
 )
 
-func HttpSend(method, url string, header, queryData map[string]string, body map[string]interface{}) (ret map[string]interface{}, err error) {
+// Send 发送http请求
+func Send(method, url string, header map[string]string, body string) (ret map[string]interface{}, err error) {
 	var req *http.Request
 	ret = make(map[string]interface{}, 3)
 
 	client := &http.Client{}
 
-	if queryData != nil {
-		url += "?"
-		querySlice := make([]string, 0, len(queryData))
-		for k, v := range queryData {
-			querySlice = append(querySlice, k+"="+v)
-		}
-		url += strings.Join(querySlice, "&")
-	}
-
-	//请求数据
-	byteDates, err := json.Marshal(body)
-	if err != nil {
-		err = errors.Wrap(err, "request data to json err")
-		return
-	}
-	reader := bytes.NewReader(byteDates)
+	reader := bytes.NewReader([]byte(body))
 
 	//构建req
 	req, err = http.NewRequest(method, url, reader)
 	if err != nil {
-		err = errors.Wrap(err, "structure http err")
+		err = errors.Wrap(err, "util structure http err:")
 		return
 	}
 
 	//设置请求header
 	for k, v := range header {
-		//req.Header.Add("content-type", "application/json")
 		req.Header.Add(k, v)
 	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	//发送请求
 	resp, err := client.Do(req)
 	if err != nil {
-		err = errors.Wrap(err, "send http err：")
+		err = errors.Wrap(err, "util http send err:")
 		return
 	}
 	defer resp.Body.Close()
 
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		err = errors.Wrap(err, "read body err：")
+		err = errors.Wrap(err, "util http read body err：")
 		return
 	}
 
 	ret["http_code"] = resp.StatusCode
-	ret["response"] = make(map[string]interface{})
 
 	if resp.StatusCode != http.StatusOK {
-		ret["msg"] = "http code:" + strconv.Itoa(resp.StatusCode)
+		str := fmt.Sprintf("http code is %d", resp.StatusCode)
+		err = errors.New(str)
+		return
 	}
 
 	if b != nil {
-		bStr := string(b)
-		bMap, err := conversion.JsonToMap(bStr)
-		if err != nil {
-			ret["response"] = string(b)
-		} else {
-			ret["response"] = bMap
-		}
+		ret["response"] = string(b)
 	}
 
 	return
