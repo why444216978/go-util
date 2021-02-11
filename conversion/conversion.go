69a6b0f8ed36ab49c5d256f14e5d75af0ee99191
weihaoyu <weihaoyu@ymt360.com>
2021-02-11 15:34:44 +0800

    conversion

--- conversion/conversion.go
+++ conversion/conversion.go
@@ -6,19 +6,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 	"unsafe"
-
-	util_err "github.com/why444216978/go-util/error"
 )
 
 // DeepCopy 深拷贝转换
-// type User struct {
-// 	A string
-// }
-// user1 := &User{A:"a"}
-// user2 := new(User)
-// conversion.DeepCopy(user2, user1)
+//	 type User struct {
+//		 A string
+//	 }
+//	 user1 := &User{A:"a"}
+//	 user2 := new(User)
+//	 conversion.DeepCopy(user2, user1)
 func DeepCopy(dst, src interface{}) error {
 	var buf bytes.Buffer
 	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
@@ -27,58 +26,73 @@ func DeepCopy(dst, src interface{}) error {
 	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
 }
 
-// JsonEncode interface转json
-func JsonEncode(v interface{}) string {
+// JsonEncode interface格式化为json
+func JsonEncode(v interface{}) (string, error) {
 	b, err := json.Marshal(v)
-	util_err.Must(err)
-	return string(b)
+	return string(b), err
 }
 
-// MapToJsonInt 索引为int的map转json
-func MapToJsonInt(data map[int]interface{}) string {
+// MapToJsonInt key为int的map转json
+func MapToJsonInt(data map[int]interface{}) (string, error) {
 	jsonStr, err := json.Marshal(data)
-	util_err.Must(err)
-	return string(jsonStr)
+	return string(jsonStr), err
 }
 
-// MapToJson 索引为string的map转json
-func MapToJson(data map[string]interface{}) string {
+// MapToJson key为string的map转json
+func MapToJson(data map[string]interface{}) (string, error) {
 	jsonStr, err := json.Marshal(data)
-	util_err.Must(err)
-	return string(jsonStr)
+	return string(jsonStr), err
 }
 
 // JsonToMapArray json转map数组
-func JsonToMapArray(data string) []map[string]interface{} {
+func JsonToMapArray(data string) ([]map[string]interface{}, error) {
 	var res []map[string]interface{}
 	if data == "" {
-		return res
+		return res, nil
 	}
 	err := json.Unmarshal([]byte(data), &res)
-	util_err.Must(err)
 
-	return res
+	return res, err
 }
 
 // JsonToMap json转map
-func JsonToMap(data string) map[string]interface{} {
+func JsonToMap(data string) (map[string]interface{}, error) {
 	var res map[string]interface{}
 	if data == "" {
-		return res
+		return res, nil
 	}
 	err := json.Unmarshal([]byte(data), &res)
-	util_err.Must(err)
-	return res
+
+	return res, err
 }
 
-// StructToMap 结构体转map
+// StructToMap struct转map
 func StructToMap(obj interface{}) map[string]interface{} {
 	obj1 := reflect.TypeOf(obj)
 	obj2 := reflect.ValueOf(obj)
 
 	var data = make(map[string]interface{})
 	for i := 0; i < obj1.NumField(); i++ {
-		data[strings.ToLower(obj1.Field(i).Name)] = obj2.Field(i).Interface()
+		k := obj1.Field(i).Tag.Get("json")
+		if k == "" {
+			k = obj1.Field(i).Name
+		}
+		data[strings.ToLower(k)] = obj2.Field(i).Interface()
+	}
+	return data
+}
+
+// GetStructJson 获得结构体的json切片
+func GetStructJson(obj interface{}) []string {
+	obj1 := reflect.TypeOf(obj)
+
+	var data = make([]string, obj1.NumField())
+	for i := 0; i < obj1.NumField(); i++ {
+		k := obj1.Field(i).Tag.Get("json")
+		if k == "" {
+			k = obj1.Field(i).Name
+		}
+		data = append(data, k)
 	}
 	return data
 }
@@ -118,8 +132,8 @@ func StructToMapByReflect(v interface{}) string {
 }
 
 // StringToByte 自定义string转byte
-// 解决强转效率低：先分配一个内存再复制内容
-// StringToByte("str"
+// 解决强转内存分配效率低问题
+//	 StringToByte("str")
 func StringToByte(str string) []byte {
 	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&str))
 	sliceHeader.Cap = sliceHeader.Len
@@ -129,14 +143,147 @@ func StringToByte(str string) []byte {
 
 // ByteToString 自定义byte转string
 // 解决强转效率低：先分配一个内存再复制内容
-// ByteToString([]byte("why"))
+//	 ByteToString([]byte("why"))
 func ByteToString(b []byte) string {
 	ptr := (*string)(unsafe.Pointer(&b))
 	return *ptr
 }
 
-//json转struct
-//jsonStr := `{"name":"why","age":18}`
-//user := User{}
-//json.Unmarshal([]byte(jsonStr), &user)
-//fmt.Println(user)
+// StringToUint8 string转uint
+func StringToUint8(str string) uint8 {
+	intTmp, _ := strconv.Atoi(str)
+	return uint8(intTmp)
+}
+
+// StringToUint16 string转uint16
+func StringToUint16(str string) uint16 {
+	intTmp, _ := strconv.Atoi(str)
+	return uint16(intTmp)
+}
+
+// StringToUint64 string转uint64
+func StringToUint64(str string) uint64 {
+	intTmp, _ := strconv.Atoi(str)
+	return uint64(intTmp)
+}
+
+// StringToInt8 string转int8
+func StringToInt8(str string) int8 {
+	intTmp, _ := strconv.Atoi(str)
+	return int8(intTmp)
+}
+
+// StringToInt16 string转int16
+func StringToInt16(str string) int16 {
+	intTmp, _ := strconv.Atoi(str)
+	return int16(intTmp)
+}
+
+// StringToInt32 string转int32
+func StringToInt32(str string) int32 {
+	intTmp, _ := strconv.Atoi(str)
+	return int32(intTmp)
+}
+
+// StringToInt64 string转int64
+func StringToInt64(str string) int64 {
+	intTmp, _ := strconv.Atoi(str)
+	return int64(intTmp)
+}
+
+// InterfaceStringToUint8 interface转uint8
+func InterfaceStringToUint8(str interface{}) uint8 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return uint8(intTmp)
+}
+
+// InterfaceStringToUint16 interface转uint16
+func InterfaceStringToUint16(str interface{}) uint16 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return uint16(intTmp)
+}
+
+// InterfaceStringToUint64 interface转uint64
+func InterfaceStringToUint64(str interface{}) uint64 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return uint64(intTmp)
+}
+
+// InterfaceStringToInt8 interface转int8
+func InterfaceStringToInt8(str interface{}) int8 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return int8(intTmp)
+}
+
+// InterfaceStringToInt16 interface转int16
+func InterfaceStringToInt16(str interface{}) int16 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return int16(intTmp)
+}
+
+// InterfaceStringToInt32 interface转in32
+func InterfaceStringToInt32(str interface{}) int32 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return int32(intTmp)
+}
+
+// InterfaceStringToInt64 interface转int64
+func InterfaceStringToInt64(str interface{}) int64 {
+	strTmp := str.(string)
+	intTmp, _ := strconv.Atoi(strTmp)
+	return int64(intTmp)
+}
+
+// InterfaceFloat64ToUint8 interface转uint8
+func InterfaceFloat64ToUint8(str interface{}) uint8 {
+	tmp := str.(float64)
+	return uint8(tmp)
+}
+
+// InterfaceFloat64ToUint16 interface转uint16
+func InterfaceFloat64ToUint16(str interface{}) uint16 {
+	tmp := str.(float64)
+	return uint16(tmp)
+}
+
+// InterfaceFloat64ToUint32 interface转uint32
+func InterfaceFloat64ToUint32(str interface{}) uint32 {
+	tmp := str.(float64)
+	return uint32(tmp)
+}
+
+// InterfaceFloat64ToUint64 interface转uint64
+func InterfaceFloat64ToUint64(str interface{}) uint64 {
+	tmp := str.(float64)
+	return uint64(tmp)
+}
+
+// InterfaceFloat64ToInt8 interface转int8
+func InterfaceFloat64ToInt8(str interface{}) int8 {
+	tmp := str.(float64)
+	return int8(tmp)
+}
+
+// InterfaceFloat64ToInt16 interface转int16
+func InterfaceFloat64ToInt16(str interface{}) int16 {
+	tmp := str.(float64)
+	return int16(tmp)
+}
+
+//InterfaceFloat64ToInt32 interface转int32
+func InterfaceFloat64ToInt32(str interface{}) int32 {
+	tmp := str.(float64)
+	return int32(tmp)
+}
+
+// InterfaceFloat64ToInt64 interface转in64
+func InterfaceFloat64ToInt64(str interface{}) int64 {
+	tmp := str.(float64)
+	return int64(tmp)
+}
