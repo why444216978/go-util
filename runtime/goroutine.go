8510bf8acd9df111e3efe5bc94e34a39c674f4c2
weihaoyu <weihaoyu@ymt360.com>
2021-03-13 17:24:04 +0800

    add runtime util

---
+++ runtime/goroutine.go
@@ -0,0 +1,12 @@
+package runtime
+
+// GetGoroutineId 获得当前 goroutine id
+func GetGoroutineId() (int, error) {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	stk := string(buf[:n])
+
+	str := stk[10:11]
+
+	return strconv.Atoi(str)
+}
