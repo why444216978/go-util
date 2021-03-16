9287758ae4237570cf6b3e91583dca466aa1997f
weihaoyu <weihaoyu@ymt360.com>
2021-03-16 22:42:43 +0800

    orm

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/google/uuid v1.1.5
 	github.com/pkg/errors v0.9.1
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gorm.io/gorm v1.21.3
 )
--- orm/orm.go
+++ orm/orm.go
@@ -1,8 +1,86 @@
 package orm
 
 import (
+	"context"
 	"fmt"
+
+	"gorm.io/gorm"
 )
+
+// Count 数量
+func Count(ctx context.Context, db *gorm.DB, model interface{}, where map[string]interface{}) (count int64, err error) {
+	db = db.Model(model)
+
+	for k, v := range where {
+		db.Where(k, v)
+	}
+
+	err = db.Count(&count).Error
+
+	return
+}
+
+// Select 查询
+func Select(ctx context.Context, db *gorm.DB, model interface{}, fields string, where map[string]interface{}, group []string, groupData interface{}, having map[string]interface{}, order []string, start, limit int) (err error) {
+	db = db.Model(model).Select(fields)
+
+	for k, v := range where {
+		db.Where(k, v)
+	}
+
+	for _, v := range group {
+		db.Group(v)
+	}
+
+	for k, v := range having {
+		db.Having(k, v)
+	}
+
+	for _, v := range order {
+		db.Order(v)
+	}
+
+	db = db.Offset(start).Limit(limit)
+
+	if groupData != nil {
+		err = db.Find(groupData).Error
+	} else {
+		err = db.Find(model).Error
+	}
+
+	return
+}
+
+// Update 更新
+func Update(ctx context.Context, db *gorm.DB, model interface{}, where map[string]interface{}, updates map[string]interface{}) (int64, error) {
+	db = db.Model(model)
+
+	for k, v := range where {
+		db.Where(k, v)
+	}
+
+	res := db.Updates(updates)
+
+	return res.RowsAffected, res.Error
+}
+
+// Insert 插入
+func Insert(ctx context.Context, db *gorm.DB, model interface{}) (int64, error) {
+	res := db.Create(model)
+
+	return res.RowsAffected, res.Error
+}
+
+// Delete 删除
+func Delete(ctx context.Context, db *gorm.DB, model interface{}, where map[string]interface{}) (int64, error) {
+	for k, v := range where {
+		db.Where(k, v)
+	}
+
+	res := db.Delete(model)
+
+	return res.RowsAffected, res.Error
+}
 
 // FormatEqString 格式化string等于
 func FormatEqString(m map[string]string) map[string]interface{} {
