ed403354cfaa0b0a5c0c54ee385a23c5d9723e38
weihaoyu <444216978@qq.com>
2022-09-19 15:15:14 +0800

    wrap panic

---
+++ panic/panic.go
@@ -0,0 +1,31 @@
+package panic
+
+import (
+	"bytes"
+	"fmt"
+	"runtime/debug"
+)
+
+// panicError https://cs.opensource.google/go/x/sync/+/036812b2:singleflight/singleflight.go
+type panicError struct {
+	value interface{}
+	stack []byte
+}
+
+// Error implements error interface.
+func (p *panicError) Error() string {
+	return fmt.Sprintf("%v\n\n%s", p.value, p.stack)
+}
+
+// NewPanicError is format panic error
+func NewPanicError(v interface{}) error {
+	stack := debug.Stack()
+
+	// The first line of the stack trace is of the form "goroutine N [status]:"
+	// but by the time the panic reaches Do the goroutine may no longer exist
+	// and its status will have changed. Trim out the misleading line.
+	if line := bytes.IndexByte(stack[:], '\n'); line >= 0 {
+		stack = stack[line+1:]
+	}
+	return &panicError{value: v, stack: stack}
+}
