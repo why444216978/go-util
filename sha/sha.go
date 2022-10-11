f90deef6657f4d0a3c5f5dfc915b14e82362a86c
weihaoyu <444216978@qq.com>
2022-10-11 17:54:45 +0800

    change sha

--- sha/sha.go
+++ sha/sha.go
@@ -1,9 +1,11 @@
-package sha256
+package sha
 
 import (
 	"crypto/hmac"
+	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"io"
 )
 
 // Sha256String
@@ -21,4 +23,21 @@ func Sha256(key, data []byte) []byte {
 	mac := hmac.New(sha256.New, key)
 	mac.Write(data)
 	return mac.Sum(nil)
+}
+
+func Sha1Hex(str string) (string, error) {
+	b, err := Sha1(str)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
+
+func Sha1(str string) ([]byte, error) {
+	h := sha1.New()
+	if _, err := io.WriteString(h, str); err != nil {
+		return nil, err
+	}
+
+	return h.Sum(nil), nil
 }
