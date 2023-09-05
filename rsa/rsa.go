aede5563b383d1f388a57ecc1fe618325bdb0ed7
weihaoyu <444216978@qq.com>
2023-09-05 14:23:14 +0800

    RSA PKCS8

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -6,72 +6,56 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
-	"os"
 )
 
 // PublicEncrypt 公钥加密
-func PublicEncrypt(encryptStr string, path string) (string, error) {
-	// 打开文件
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	// 读取文件内容
-	info, _ := file.Stat()
-	buf := make([]byte, info.Size())
-	file.Read(buf)
-
+func PublicEncrypt(encryptStr string, publicKey []byte) (result string, err error) {
 	// pem 解码
-	block, _ := pem.Decode(buf)
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return "", ErrBadPem
+	}
 
 	// x509 解码
 	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		return "", err
+		return
 	}
+	pub := publicKeyInterface.(*rsa.PublicKey)
 
-	// 类型断言
-	publicKey := publicKeyInterface.(*rsa.PublicKey)
-
-	//对明文进行加密
-	encryptedStr, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, []byte(encryptStr))
+	// 对明文进行加密
+	encryptedStr, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(encryptStr))
 	if err != nil {
-		return "", err
+		return
 	}
 
-	//返回密文
+	// 返回密文
 	return base64.URLEncoding.EncodeToString(encryptedStr), nil
 }
 
 // PrivateDecrypt 私钥解密
-func PrivateDecrypt(decryptStr string, path string) (string, error) {
-	// 打开文件
-	file, err := os.Open(path)
+func PrivateDecrypt(decryptStr string, privateKey []byte) (result string, err error) {
+	decryptBytes, err := base64.URLEncoding.DecodeString(decryptStr)
 	if err != nil {
-		return "", err
+		return
 	}
-	defer file.Close()
-
-	// 获取文件内容
-	info, _ := file.Stat()
-	buf := make([]byte, info.Size())
-	file.Read(buf)
 
 	// pem 解码
-	block, _ := pem.Decode(buf)
+	block, _ := pem.Decode(privateKey)
 
-	// X509 解码
-	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	// X509-PKCS8 解码
+	_priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
-		return "", err
+		return
 	}
-	decryptBytes, err := base64.URLEncoding.DecodeString(decryptStr)
+	priv := _priv.(*rsa.PrivateKey)
 
-	//对密文进行解密
-	decrypted, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, decryptBytes)
+	// 对密文进行解密
+	decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, priv, decryptBytes)
+	if err != nil {
+		return
+	}
 
-	//返回明文
+	// 返回明文
 	return string(decrypted), nil
 }
--- rsa/sign.go
+++ rsa/sign.go
@@ -11,9 +11,7 @@ import (
 	"errors"
 )
 
-var (
-	ErrBadPem = errors.New("密钥错误")
-)
+var ErrBadPem = errors.New("密钥错误")
 
 func Sign(data string, rsaPrivateKey []byte) (string, error) {
 	block, _ := pem.Decode(rsaPrivateKey)
@@ -26,10 +24,11 @@ func Sign(data string, rsaPrivateKey []byte) (string, error) {
 	}
 
 	hash := crypto.Hash.New(crypto.SHA1)
-	hash.Write([]byte(data))
+	if _, err := hash.Write([]byte(data)); err != nil {
+		return "", err
+	}
 
 	sign, err := privKey.Sign(rand.Reader, hash.Sum(nil), crypto.SHA1)
-
 	if err != nil {
 		return "", err
 	}
@@ -41,19 +40,21 @@ func CheckSign(data, target string, publicKey []byte) error {
 	if err != nil {
 		return err
 	}
-	// public, _ := base64.StdEncoding.DecodeString(pubKey)
 
 	block, _ := pem.Decode(publicKey)
 	if block == nil {
 		return ErrBadPem
 	}
-	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	pubInterface, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
 		return err
 	}
 	pub := pubInterface.(*rsa.PublicKey)
 
 	hash := sha1.New()
-	hash.Write([]byte(data))
+	if _, err := hash.Write([]byte(data)); err != nil {
+		return err
+	}
+
 	return rsa.VerifyPKCS1v15(pub, crypto.SHA1, hash.Sum(nil), sign)
 }
