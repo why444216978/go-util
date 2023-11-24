ef3547bd1fc1c239d5d73bd68f951c61dfefb07e
weihaoyu <444216978@qq.com>
2023-11-24 16:12:57 +0800

    optimize xss ugc, support inject policy

--- xss/xss.go
+++ xss/xss.go
@@ -4,10 +4,52 @@ import (
 	"github.com/microcosm-cc/bluemonday"
 )
 
-func FilterOnlyText(s string) string {
-	return bluemonday.StrictPolicy().Sanitize(s)
+var (
+	defaultStrict = bluemonday.StrictPolicy()
+	defaultUGC    = func() *bluemonday.Policy {
+		p := bluemonday.UGCPolicy()
+		p.RequireNoFollowOnLinks(false)
+		p.AllowAttrs("style").Globally()
+		p.AllowAttrs("data-origin").OnElements("img")
+		p.AllowAttrs("data-size").OnElements("img")
+		p.AllowAttrs("target").OnElements("a")
+		p.AllowAttrs("rel").OnElements("a")
+		return p
+	}()
+)
+
+type options struct {
+	policy *bluemonday.Policy
 }
 
-func FilterSaveTag(s string) string {
-	return bluemonday.UGCPolicy().Sanitize(s)
+type Option func(*options)
+
+func WithPolicy(p *bluemonday.Policy) Option {
+	return func(o *options) { o.policy = p }
+}
+
+func FilterOnlyText(s string, opts ...Option) string {
+	o := handleOptions(opts...)
+	if o.policy != nil {
+		return o.policy.Sanitize(s)
+	}
+
+	return defaultStrict.Sanitize(s)
+}
+
+func FilterSaveTag(s string, opts ...Option) string {
+	o := handleOptions(opts...)
+	if o.policy != nil {
+		return o.policy.Sanitize(s)
+	}
+
+	return defaultUGC.Sanitize(s)
+}
+
+func handleOptions(opts ...Option) *options {
+	option := &options{}
+	for _, o := range opts {
+		o(option)
+	}
+	return option
 }
--- xss/xss_test.go
+++ xss/xss_test.go
@@ -12,6 +12,32 @@ func TestFilterOnlyText(t *testing.T) {
 	assert.Equal(t, "我的2022", FilterOnlyText(s))
 }
 
+// nolint
+var equalHTML = []string{
+	`<p>正文</p>`,
+	`<p><strong style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><em><s><u>文字样式</u></s></em></strong><sub style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>1</u></s></em></strong></sub><sup style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>2</u></s></em></strong></sup></p>`,
+	`<ol><li><sup style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>1</u></s></em></strong></sup></li><li><sup style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>2</u></s></em></strong></sup></li></ol>`,
+	`<ul><li><sup style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>1</u></s></em></strong></sup></li><li><sup style="color: rgb(230, 0, 0); background-color: rgb(153, 51, 255);"><strong><em><s><u>2</u></s></em></strong></sup></li></ul>`,
+	`<p><a href="https://drs.baidu.com/dcp/editor" rel="noopener noreferrer" target="_blank">百度一下</a></p>`,
+	`<img src="https://health-community.cdn.bcebos.com/image/2023-11/23/14/2fnyMBi6dR09A/12ace5ac61f90253bdd3e2f298af3ece.png?x-bce-process=image/auto-orient,o_1/quality,Q_60" data-origin="https://health-community.cdn.bcebos.com/image/2023-11/23/14/2fnyMBi6dR09A/12ace5ac61f90253bdd3e2f298af3ece.png" data-size="151929">`,
+	`<p><br></p>`,
+}
+
+// nolint
+var filterHTML = []string{
+	`<script>alert(111)</script>`,
+	`<p>1<a href='http://www.baidu.com'>23</a></p>`,
+	`<p>1<a href='www"onmouseover="alert(1)'>23</a></p>`,
+	`<img onload='alert("aaa")' onerror=alert(111) src="https://health-community.bj.bcebos.com/image/2023-01/31/17/vtUJR28LTCHzAeeeB/100617641dca342ca1d8f7433b8d9736.jpeg" data-origin="https://health-community.bj.bcebos.com/image/2023-01/31/17/vtUJR28LTCHzAeeeB/100617641dca342ca1d8f7433b8d9736.jpeg" data-size="1010404">`,
+}
+
 func TestFilterSaveTag(t *testing.T) {
-	assert.Equal(t, `<p>我的2022</p><img src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png">`, FilterSaveTag(s))
+	for _, s := range equalHTML {
+		assert.Equal(t, true, s == FilterSaveTag(s))
+	}
+	for _, s := range filterHTML {
+		assert.Equal(t, false, s == FilterSaveTag(s))
+	}
+
+	assert.Equal(t, "我的2022", FilterSaveTag(s, WithPolicy(defaultStrict)))
 }
