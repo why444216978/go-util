6a1a66a004053e7d4942485d183c0bb50a7fd8d5
weihaoyu <444216978@qq.com>
2025-03-07 02:39:13 +0800

    decimal

--- decimal/decimal.go
+++ decimal/decimal.go
@@ -3,78 +3,206 @@ package decimal
 import (
 	"fmt"
 	"math"
-	"strconv"
+	"strings"
 
 	"github.com/shopspring/decimal"
 	"github.com/spf13/cast"
 )
 
-func DecimalFormat(v float64, n int) string {
-	return ValueFormat(TransferDecimal(v, n))
+var unicodeNum = map[string]string{
+	"0": "\u2080",
+	"1": "\u2081",
+	"2": "\u2082",
+	"3": "\u2083",
+	"4": "\u2084",
+	"5": "\u2085",
+	"6": "\u2086",
+	"7": "\u2087",
+	"8": "\u2088",
+	"9": "\u2089",
 }
 
-func TransferDecimalString(v float64, n int) string {
+func FormatPrice(v float64) string {
+	vs := cast.ToString(v)
+	arr := strings.Split(vs, ".")
+	if len(arr) != 2 {
+		return vs
+	}
+
+	if !strings.HasPrefix(arr[1], "000") {
+		return strings.TrimRight(DivFloorString(v, 4), "0")
+	}
+
+	r := []byte{}
+	stopZero := false
+	zeroCount := 0
+	for _, v := range []byte(arr[1]) {
+		if v != 0x30 {
+			stopZero = true
+		}
+
+		if !stopZero {
+			zeroCount++
+		} else {
+			r = append(r, v)
+		}
+	}
+
+	z := ""
+	for _, v := range cast.ToString(zeroCount) {
+		z += unicodeNum[string(v)]
+	}
+
+	n := len(r)
+	if n > 4 {
+		n = 4
+	}
+
+	result := fmt.Sprintf("%s.0%s%s", arr[0], z, r[:n])
+
+	return strings.TrimRight(result, "0")
+}
+
+func FormatCountBuySell(v float64) string {
+	if v < 0 {
+		return fmt.Sprintf("-%s", FormatKMB(v, 2))
+	}
+	return fmt.Sprintf("+%s", FormatKMB(v, 2))
+}
+
+func FormatDecimal(v float64, n int32) string {
+	return FormatKMB(TransferDecimal(v, n), 2)
+}
+
+func FormatDecimalVolume(v float64, n int32) string {
+	return FormatVolume(TransferDecimal(v, n))
+}
+
+func TransferDecimalString(v float64, n int32) string {
 	return cast.ToString(TransferDecimal(v, n))
 }
 
-func TransferDecimal(v float64, n int) float64 {
+func TransferDecimal(v float64, n int32) float64 {
 	if n <= 0 {
 		return v
 	}
 
 	if n <= 2 {
-		r, _ := decimal.NewFromFloat(v).Div(decimal.NewFromFloat(math.Pow10(n))).Float64()
+		r, _ := decimal.NewFromFloat(v).Div(decimal.NewFromFloat(math.Pow10(int(n)))).Float64()
 		return r
 	}
 
-	r, _ := decimal.NewFromFloat(v).Div(decimal.NewFromFloat(math.Pow10(n - 2))).Float64()
-	r = math.Floor(r)
-	r, _ = decimal.NewFromFloat(r).Div(decimal.NewFromFloat(100.0)).Float64()
+	d := decimal.NewFromFloat(v).Div(decimal.NewFromFloat(math.Pow10(int(n) - 2)))
+	d = d.Floor()
+	r, _ := d.Div(decimal.NewFromFloat(100.0)).Float64()
 	return r
 }
 
 func DivFloorFloat64(v float64, n int) float64 {
-	return divFloor(v, n)
+	return cast.ToFloat64(DivFloorString(v, n))
 }
 
 func DivFloorString(v float64, n int) string {
-	return strconv.FormatFloat(divFloor(v, n), 'f', n, 64)
+	// 1234.5678 => 1234.56
+
+	arr := strings.Split(cast.ToString(v), ".")
+
+	f := ""
+	if len(arr) == 1 {
+		for i := 1; i <= n; i++ {
+			f += "0"
+		}
+	} else {
+		l := len(arr[1])
+		for i := 0; i < n; i++ {
+			if i < l {
+				f += string(arr[1][i])
+			} else {
+				f += "0"
+			}
+		}
+	}
+
+	return fmt.Sprintf("%s.%s", arr[0], f)
 }
 
-func divFloor(v float64, n int) float64 {
-	// 1234.5678 => 123456.78
-	r, _ := decimal.NewFromFloat(v).Mul(decimal.NewFromFloat(math.Pow10(n))).Float64()
-	// 123456.78 => 123456
-	r = math.Floor(r)
-	// 123456 => 1234.56
-	d, _ := decimal.NewFromFloat(r).Div(decimal.NewFromFloat(math.Pow10(n))).Float64()
-	return d
-}
+func FormatKMB(v float64, n int) string {
+	if v == 0 {
+		return "0.00"
+	}
 
-func ValueFormat(v float64) string {
 	if v < 1000 {
 		return DivFloorString(v, 2)
 	}
 
 	if v < 1000000 {
-		return fmt.Sprintf("%.2fK", TransferDecimal(v, 3))
+		return fmt.Sprintf("%sK", DivFloorString(TransferDecimal(v, 3), n))
 	}
 
 	if v < 1000000000 {
-		return fmt.Sprintf("%.2fM", TransferDecimal(v, 6))
+		return fmt.Sprintf("%sM", DivFloorString(TransferDecimal(v, 6), n))
 	}
 
 	if v < 1000000000000 {
-		return fmt.Sprintf("%.2fB", TransferDecimal(v, 9))
+		return fmt.Sprintf("%sB", DivFloorString(TransferDecimal(v, 9), n))
 	}
 
 	if v < 1000000000000000 {
-		return fmt.Sprintf("%.2fT", TransferDecimal(v, 12))
+		return fmt.Sprintf("%sT", DivFloorString(TransferDecimal(v, 12), n))
 	}
 
 	if v < 1000000000000000000 {
-		return fmt.Sprintf("%.2fQ", TransferDecimal(v, 15))
+		return fmt.Sprintf("%sQ", DivFloorString(TransferDecimal(v, 15), n))
 	}
 
-	return fmt.Sprintf("%.2fS", TransferDecimal(v, 18))
+	return fmt.Sprintf("%sS", DivFloorString(TransferDecimal(v, 18), n))
+}
+
+func FormatVolume(v float64) string {
+	if v < 0 && math.Abs(v) < 0.01 {
+		return "< $-0.01"
+	}
+
+	if v < 0 {
+		return fmt.Sprintf("$%s", DivFloorString(v, 2))
+	}
+
+	if v == 0 {
+		return "$0.00"
+	}
+
+	if v < 0.01 {
+		return "< $0.01"
+	}
+
+	return fmt.Sprintf("$%s", FormatKMB(v, 2))
+}
+
+func FormatPercent(v float64, withSymbol bool) string {
+	symbol := ""
+	if withSymbol {
+		symbol = "+"
+	}
+
+	if v < 0 && math.Abs(v) < 0.01 {
+		return "< -0.01%"
+	}
+
+	if v < 0 {
+		return fmt.Sprintf("%s", DivFloorString(v, 2)) + "%"
+	}
+
+	if v < 0.01 {
+		return fmt.Sprintf("< %s0.01", symbol) + "%"
+	}
+
+	if v < 10 {
+		return fmt.Sprintf("%s%s", symbol, DivFloorString(v, 2)) + "%"
+	}
+
+	if v > 10 && v < 1000 {
+		return fmt.Sprintf("%s%s", symbol, DivFloorString(v, 1)) + "%"
+	}
+
+	return fmt.Sprintf("%s%s", symbol, FormatKMB(v, 1)) + "%"
 }
--- decimal/decimal_test.go
+++ decimal/decimal_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFormatPrice(t *testing.T) {
+	r := FormatPrice(0.001)
+	assert.Equal(t, "0.001", r)
+	r = FormatPrice(0.0001)
+	assert.Equal(t, "0.0₃1", r)
+	r = FormatPrice(0.00010)
+	assert.Equal(t, "0.0₃1", r)
+	r = FormatPrice(0.00012345)
+	assert.Equal(t, "0.0₃1234", r)
+	r = FormatPrice(10.00000001234)
+	assert.Equal(t, "10.0₇1234", r)
+	r = FormatPrice(10.000000012345)
+	assert.Equal(t, "10.0₇1234", r)
+}
+
 func TestTransferDecimal(t *testing.T) {
 	res := TransferDecimal(float64(88), 1)
 	assert.Equal(t, 8.8, res)
@@ -49,29 +64,74 @@ func TestDivFloorString(t *testing.T) {
 	assert.Equal(t, "1234.00", r)
 }
 
-func TestValueFormat(t *testing.T) {
+func TestFormatKMB(t *testing.T) {
 	convey.Convey("TestValueFormat", t, func() {
 		convey.Convey("< 1000", func() {
-			assert.Equal(t, "888.00", ValueFormat(888))
-			assert.Equal(t, "888.12", ValueFormat(888.123456789))
+			assert.Equal(t, "888.00", FormatKMB(888, 2))
+			assert.Equal(t, "888.12", FormatKMB(888.123456789, 2))
 		})
 		convey.Convey("< 1000000", func() {
-			assert.Equal(t, "888.88K", ValueFormat(888888))
+			assert.Equal(t, "888.88K", FormatKMB(888888, 2))
 		})
 		convey.Convey("< 1000000000", func() {
-			assert.Equal(t, "888.88M", ValueFormat(888888888))
+			assert.Equal(t, "888.88M", FormatKMB(888888888, 2))
 		})
 		convey.Convey("< 1000000000000", func() {
-			assert.Equal(t, "888.88B", ValueFormat(888888888888))
+			assert.Equal(t, "888.88B", FormatKMB(888888888888, 2))
 		})
 		convey.Convey("< 1000000000000000", func() {
-			assert.Equal(t, "888.88T", ValueFormat(888888888888888))
+			assert.Equal(t, "888.88T", FormatKMB(888888888888888, 2))
 		})
 		convey.Convey("< 1000000000000000000", func() {
-			assert.Equal(t, "888.88Q", ValueFormat(888888888888888888))
+			assert.Equal(t, "888.88Q", FormatKMB(888888888888888888, 2))
 		})
 		convey.Convey("default", func() {
-			assert.Equal(t, "888.88S", ValueFormat(888888888888888888888))
+			assert.Equal(t, "888.88S", FormatKMB(888888888888888888888, 2))
 		})
 	})
+}
+
+func TestFormatVolume(t *testing.T) {
+	convey.Convey("TestFormatVolume", t, func() {
+		convey.Convey("abs <0.01", func() {
+			assert.Equal(t, "< $-0.01", FormatVolume(-0.001))
+		})
+		convey.Convey("< 0", func() {
+			assert.Equal(t, "$-0.10", FormatVolume(-0.1))
+		})
+		convey.Convey("== 0", func() {
+			assert.Equal(t, "$0.00", FormatVolume(0))
+		})
+		convey.Convey("< 0.01", func() {
+			assert.Equal(t, "< $0.01", FormatVolume(0.001))
+		})
+		convey.Convey("KMB", func() {
+			assert.Equal(t, "$1.11K", FormatVolume(1111.11))
+		})
+	})
+}
+
+func TestFormatPercent(t *testing.T) {
+	r := FormatPercent(-0.888, false)
+	assert.Equal(t, "-0.88%", r)
+
+	r = FormatPercent(0.001, false)
+	assert.Equal(t, "< 0.01%", r)
+	r = FormatPercent(0.001, true)
+	assert.Equal(t, "< +0.01%", r)
+
+	r = FormatPercent(0.888, false)
+	assert.Equal(t, "0.88%", r)
+	r = FormatPercent(0.888, true)
+	assert.Equal(t, "+0.88%", r)
+
+	r = FormatPercent(10.888, false)
+	assert.Equal(t, "10.8%", r)
+	r = FormatPercent(10.888, true)
+	assert.Equal(t, "+10.8%", r)
+
+	r = FormatPercent(1111.111, false)
+	assert.Equal(t, "1.1K%", r)
+	r = FormatPercent(1111.111, true)
+	assert.Equal(t, "+1.1K%", r)
 }
