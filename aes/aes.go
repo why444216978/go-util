7f26b65e68ecdbd1437b073f4bd983dc038ecf7c
weihaoyu <444216978@qq.com>
2025-05-25 15:00:18 +0800

    aes

--- aes/aes.go
+++ aes/aes.go
@@ -4,53 +4,58 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"errors"
 )
 
-// DecryptECB AES-ECB 加密
-func EncryptECB(data, key []byte) ([]byte, error) {
+func EncryptECB(plaintext, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
 	blockSize := block.BlockSize()
-	data = pkcs5Padding(data, blockSize)
-	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
-	encrypted := make([]byte, len(data))
-	blockMode.CryptBlocks(encrypted, data)
-	return encrypted, nil
+	plaintext = pkcsPadding(plaintext, blockSize)
+
+	ciphertext := make([]byte, len(plaintext))
+	for bs, be := 0, blockSize; bs < len(plaintext); bs, be = bs+blockSize, be+blockSize {
+		block.Encrypt(ciphertext[bs:be], plaintext[bs:be])
+	}
+
+	return ciphertext, nil
 }
 
-// DecryptECB AES-ECB 解密
-func DecryptECB(data, key []byte) ([]byte, error) {
+func DecryptECB(ciphertext, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
 	blockSize := block.BlockSize()
-	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
-	decrypted := make([]byte, len(data))
-	blockMode.CryptBlocks(decrypted, data)
-	decrypted = pkcs5UnPadding(decrypted)
-	return decrypted, nil
+	if len(ciphertext)%blockSize != 0 {
+		return nil, errors.New("密文长度不是块大小的整数倍")
+	}
+
+	plaintext := make([]byte, len(ciphertext))
+	for bs, be := 0, blockSize; bs < len(ciphertext); bs, be = bs+blockSize, be+blockSize {
+		block.Decrypt(plaintext[bs:be], ciphertext[bs:be])
+	}
+
+	return pkcsUnPadding(plaintext), nil
 }
 
-// EncryptCBC AES-CBC 加密
 func EncryptCBC(data, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	blockSize := block.BlockSize()
-	data = pkcs5Padding(data, blockSize)
+	data = pkcsPadding(data, blockSize)
 	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
 	encrypted := make([]byte, len(data))
 	blockMode.CryptBlocks(encrypted, data)
 	return encrypted, nil
 }
 
-// DecryptCBC AES-CBC 解密
 func DecryptCBC(data, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -60,17 +65,17 @@ func DecryptCBC(data, key []byte) ([]byte, error) {
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
 	decrypted := make([]byte, len(data))
 	blockMode.CryptBlocks(decrypted, data)
-	decrypted = pkcs5UnPadding(decrypted)
+	decrypted = pkcsUnPadding(decrypted)
 	return decrypted, nil
 }
 
-func pkcs5Padding(cipherText []byte, blockSize int) []byte {
+func pkcsPadding(cipherText []byte, blockSize int) []byte {
 	padding := blockSize - len(cipherText)%blockSize
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(cipherText, padText...)
 }
 
-func pkcs5UnPadding(decrypted []byte) []byte {
+func pkcsUnPadding(decrypted []byte) []byte {
 	length := len(decrypted)
 	unPadding := int(decrypted[length-1])
 	return decrypted[:(length - unPadding)]
